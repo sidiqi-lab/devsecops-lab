78f89788c3491442cc5c90e663f045773dbe36af
sidiqi-lab <qayum.sidiqi@gmail.com>
2026-09-04 14:15:23 -0400

    Add Terraform and Ansible infrastructure automation

---
+++ terraform/outputs.tf
@@ -0,0 +1,5 @@
+output "environment" {
+  description = "Current deployment environment"
+  value       = var.environment
+}
+
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
+
